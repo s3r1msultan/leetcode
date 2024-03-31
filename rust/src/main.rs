bf238aff1a730e5cea3a40af01ee1890cac4807b
s3r1msultan <ersultanserimbetov@gmail.com>
2024-03-31 15:44:57 +0500

    solved 535 task, but not satisfied the solution. later i will try to solve using encoding and decoding algorithms

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,6 +2,7 @@ mod task_2325;
 mod task_1512;
 mod task_1689;
 mod task_2396;
+mod task_535;
 
 fn main() {
 
--- rust/src/task_2396.rs
+++ rust/src/task_2396.rs
@@ -12,7 +12,7 @@
 // Output: false
 // Explanation: In base 2: 9 = 1001 (base 2), which is palindromic.
 // In base 3: 9 = 100 (base 3), which is not palindromic.
-// Therefore, 9 is not strictly palindromic so we return false.
+// Therefore, 9 is not strictly palindromic, so we return false.
 // Note that in bases 4, 5, 6, and 7, n = 9 is also not palindromic.
 //
 // Example 2:
