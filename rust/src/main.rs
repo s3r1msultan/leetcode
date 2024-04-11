38d45a0788e1c278faf017cd8aeefed4f60fc0b2
s3r1msultan <ersultanserimbetov@gmail.com>
2024-04-12 02:43:19 +0500

    solved 8 tasks

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,6 +8,14 @@ mod task_205;
 mod task_1;
 mod task_7;
 mod task_950;
+mod task_402;
+mod task_2413;
+mod task_83;
+mod task_82;
+mod task_509;
+mod task_1137;
+mod task_136;
+mod task_268;
 
 fn main() {
 
